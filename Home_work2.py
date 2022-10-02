9568bb4a890d84239c4f6289c967f767c67bc20c
Veronika Malinouskaya <veronika_malinov@mail.ru>
2022-10-02 07:13:27 -0700

    Задача2

--- Home_work2.py
+++ Home_work2.py
@@ -2,13 +2,13 @@
 #Пример:*
 #6782 -> 23
 #0,56 -> 11
-n = int(input("Введите число: "))
+#n = int(input("Введите число: "))
  
-suma = 0 
-while n > 0:
-    digit = n % 10
-    suma = suma + digit
-    n = n // 10
+#suma = 0 
+#while n > 0:
+    #digit = n % 10
+    #suma = suma + digit
+    #n = n // 10
  
-print("Сумма:", suma)
+#print("Сумма:", suma)
  
