d105f936873715668d15c4a5c8bfe520f5a2ab84
Veronika Malinouskaya <veronika_malinov@mail.ru>
2022-10-03 01:22:33 -0700

    Задача 4

---
+++ Home_Work3_4.py
@@ -0,0 +1,26 @@
+#Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+#Пример:
+#45 -> 101101
+#3 -> 11
+#2 -> 10
+a = int(input("Введите число -> "))
+
+x = []
+integer = []
+result = []
+
+x = list(str(a))[::-1]
+
+while True:
+    if a != 0:
+        if a % 2 == 0:
+            result.append(0)
+            a = a // 2
+        elif a % 2:
+            result.append(1)
+            a = a // 2
+    else:
+        result.reverse()
+        print(result)
+        break
+input()
