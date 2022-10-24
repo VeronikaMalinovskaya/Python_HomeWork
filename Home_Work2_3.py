69365873a5ead8bfe62ace27f40dca37d47f5c5e
Veronika Malinouskaya <veronika_malinov@mail.ru>
2022-10-24 05:17:19 -0700

    Задача 3 к 6 семинару

--- Home_Work2_3.py
+++ Home_Work2_3.py
@@ -2,11 +2,16 @@
 #Пример:*
 #Для n = 6: {1: 2, 2: 2.25, 3: 2.37, 4: 2.44, 5: 2.49, 6: 2.52}
 
-n = int(input('Введите число '))
+#n = int(input('Введите число N '))
 
-lst = [round((1+1/i)**i, 2) 
-for i in range(1, n+1)]
+#lst = [round((1+1/i)**i, 2) 
+#for i in range(1, n+1)]
 
-print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 2)}')
+#print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 2)}')
 
 
+#Вариант с использованием функции zip
+n = int(input("Введите число N: "))
+list= [i for i in range(1, n+1)]
+lst= [round ((1 + 1/i)**i, 2) for i in range (1, n+1)]
+print(f"Для N = {n}: {dict(zip(list, lst))}")
